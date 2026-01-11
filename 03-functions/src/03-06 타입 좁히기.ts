96c4508d2a8d85c1df416c9dfe2d57c96660f782
hyejoy <devchoi97@gmail.com>
2026-01-11 12:47:02 +0900

    docs: 타입 좁히기 예제 주석 보완 및 학습 코드 정리

--- 03-functions/src/03-06 타입 좁히기.ts
+++ 03-functions/src/03-06 타입 좁히기.ts
@@ -1,5 +1,5 @@
 /**
- * 타입 좁히기
+ * ⭐ 타입 좁히기
  * : 조건문 등을 이용해 넓은타입에서 좁은타입으로
  * 타입을 상황에 따라 좁히는 방법을 이야기
  */
@@ -18,7 +18,8 @@ function func(value: number | string) {
 }
 
 /**
- * 📝 (typeof value === "type") 형식을 타입가드 라고 함
+ * ⭐ 타입가드
+ *    (typeof value === "type") 형식을 타입가드 라고 함
  */
 
 /**
@@ -29,7 +30,7 @@ function func(value: number | string) {
 // typeof value === "object" 를 쓰게되면 Date, null 둘다 해당되어서 권장 x
 // ⭐ 이런경우엔 instanceof 연산자를 써주면 된다.
 // A instanceof B
-// A의 값이 B의 인스턴스인가요? 라는 뜻
+// ⭐ A의 값이 B의 인스턴스인가요? 라는 뜻
 function func1(value: string | number | Date | null) {
   if (typeof value === "number") {
     console.log(value.toFixed()); // number 타입으로 추론
@@ -53,14 +54,16 @@ function func2(value: string | number | Date | null | Person) {
   } else if (value instanceof Date) {
     console.log(value.getTime());
   }
-
   // ❌❌❌ else if (value instanceof Person)
-  // Person은 class가 아니고 객체 타입이라서 instaceof 연산자를 쓸 수 없음
+  // Person은 class가 아니고 객체 타입이라서 instanceof 연산자를 쓸 수 없음
+  // ⭐ Type과 class는 다르다
 
   /**
-   * 프로퍼티의 값 "age" 를 포함하고 있는지 확인하는 in 연산자를 사용
-   * in 뒤에는 null이나 undefined가 올수없으므로
-   * value && 를 적어주어 value가 있을때만 실행되도록 작성함
+   * ⭐ Type인 경우
+   * 객체가 "age"라는 프로퍼티 키를 가지고 있는지 확인하기 위해
+   * in 연산자를 사용함
+   * in 연산자는 우변에 null 또는 undefined가 올 수 없으므로
+   * value && 조건을 추가하여 값이 있을 때만 검사하도록 처리함
    */
   else if (value && "age" in value) {
     console.log(`${value.name}은 ${value.age}살 입니다.`);
